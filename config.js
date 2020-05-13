27c29adde55b863ea80c7c37c3131a689aa79ccf
max_tricky_lake <mdovhopo@student.unit.ua>
2020-05-13 12:41:06 +0300

    fix doc

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ const config = {
   ROOT_DIR: __dirname,
   URL_PORT: 80,
   URL_PATH: 'http://localhost',
-  BASE_VERSION: 'v2',
+  // BASE_VERSION: 'v2',
   CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
   PROJECT_DIR: __dirname,
 };
 config.OPENAPI_YAML = path.join(config.ROOT_DIR, 'api', 'openapi.yaml');
-config.FULL_PATH = `${config.URL_PATH}:${config.URL_PORT}/${config.BASE_VERSION}`;
+config.FULL_PATH = `${config.URL_PATH}:${config.URL_PORT}`;
 config.FILE_UPLOAD_PATH = path.join(config.PROJECT_DIR, 'uploaded_files');
 
 module.exports = config;
